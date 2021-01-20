9b7d7d63a59be4714f7657d145da172321af26e5
CGreenwell98 <christophergreenwell7@gmail.com>
2021-01-20 14:40:04 +0000

    v2

--- public/javascript/products.js
+++ public/javascript/products.js
@@ -2,5 +2,7 @@ const productImages = document.querySelectorAll("img");
 const productDisplayArea = document.querySelector(".product-display-area");
 const productSection = document.querySelector(".product-section");
 
-productSection.classList.remove("spinner");
-productDisplayArea.classList.remove("hidden");
+window.addEventListener("load", function () {
+  productSection.classList.remove("spinner");
+  productDisplayArea.classList.remove("hidden");
+});
